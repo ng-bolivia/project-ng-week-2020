6fa46d450a3483716b8e630287d73f29f9fd2b46
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2020-05-12 14:37:44 -0400

    :construction: work

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+
+  form = new FormGroup({});
 
   constructor() { }
 
