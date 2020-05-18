1b38b0cafff6f9046738a4d5705602a5ed480eb1
Jheison Alzate <developer.alzate@gmail.com>
2020-05-18 11:47:37 -0500

    :sparkles: SSR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'project-ng-week';
 
+  constructor(
+    // tslint:disable-next-line: ban-types
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.useLocalStore();
+  }
+
   useLocalStore() {
-    window.localStorage.setItem('save', 'test');
+    if (isPlatformBrowser(this.platformId)) {
+      window.localStorage.setItem('save', 'test');
+    }
+    if (isPlatformServer(this.platformId)) {
+      // Server only code.
+    }
   }
 }
